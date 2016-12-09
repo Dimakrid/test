57ec60c65ec547faea15d9eb41937e42491c205a
Dimakrid <dimakritkai@gmail.com>
2016-12-09 16:32:21 +0300

    1
    1

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 ﻿$(function(){
-	$('#search_form_2').submit(function(e){
+	$('#search_form').submit(function(e){
 		e.preventDefault();		
 	});	
 
 })
 
-function search2(){
-	$('#results_2').html('');		
+function search(){
+	$('#results').html('');		
 	//
-	q = $('#query_2').val();	
+	q = $('#query').val();	
 	// получить результаты апи	
 	$.get(
 	"https://www.googleapis.com/youtube/v3/search",{
 		part: 'snippet, id',
 		q: q,
 		maxResults: 20,
-		order: 'date',
+		order: 'viewCount',
 		type:'video',
 	key: 'AIzaSyB3cTEZ3iATzGW519SGw09TeWMTEfXmWfc'},
 	function(data){		
 		//	console.log(data);	
-		$step2=30;
+		$step=1;
 		$.each(data.items, function(i,item){			
-			var output2 = getOutput(item, $step2);
-			$step2=$step2+1;
-			$('#results_2').append(output2);			
+			var output = getOutput(item, $step);
+			$step=$step+1;
+			$('#results').append(output);			
 		});		
 	}
 	);
 }
 
 
-function getOutput2(item,$st2){
+function getOutput(item,$st){
 	
 	var videoId = item.id.videoId;
 	var title = item.snippet.title;
@@ -40,8 +40,8 @@ function getOutput2(item,$st2){
 	var channelTitle = item.snippet.channelTitle;
 	var videoDate = item.snippet.publishedAt;		
 	var output = '<li>'+ 	 
-	'<a id="showHideContent" class="spoiler-title" href="javascript:void(0);" onclick="test('+ "'" +'content2' +$st + ''+ "'" +')" >'+'<h3>'+title+'</h3>'+'</a>'+
-	'<dd id="content2'+ $st +'" class="asd">' + 
+	'<a id="showHideContent" class="spoiler-title" href="javascript:void(0);" onclick="test('+ "'" +'content' +$st + ''+ "'" +')" >'+'<h3>'+title+'</h3>'+'</a>'+
+	'<dd id="content'+ $st +'" class="asd">' + 
 	'<iframe width="400" height="200" src="http://www.youtube.com/embed/'+videoId+'" frameborder="0"></iframe>'+
 	'</dd>' +	
 	'<small>Автор <strong>'+channelTitle+'</strong> от '+videoDate+'</small>' +	
